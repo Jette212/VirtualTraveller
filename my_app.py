739028c225ba39be6ec847954244de53e2fae709
Jette212 <06jlutz@gmail.com>
2017-11-01 19:05:42 +0000

    add html and flask

--- my_app.py
+++ my_app.py
@@ -1,9 +1,9 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask("MyApp")
 
 @app.route("/")
 def hello():
-	return "Hello World"
+	return render_template("main.html")
 
 app.run(debug=True)
