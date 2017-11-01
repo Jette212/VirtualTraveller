5c5e04d4c0643c3969294654207811e6ade04584
Furnisha <furnisha_lycett@hotmail.co.uk>
2017-11-01 19:56:16 +0000

    Added weather API function

--- my_app.py
+++ my_app.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template
 
 app = Flask("MyApp")
 
+def what_is_weather(location):
+	import requests
+	endpoint = "http://api.openweathermap.org/data/2.5/weather"
+	payload = {"q": location, "units":"metric", "appid":"3b503bfa7ea64695d425e95faf892625"}
+	response = requests.get(endpoint, params=payload)
+	return response.json()["weather"][0]["description"] + '<br/>' + str(response.json()["main"]["temp"])
+
+
 @app.route("/")
 def hello():
 	return render_template("main.html")
 
 @app.route("/weather/<location>")
 def weather(location):
-	return "sunny!"
+	return str(what_is_weather(location))
 
 app.run(debug=True)
