3b9bcaad468a5bdbd691039fb4604b5c27f884e8
Irina <mihaela-irina.stoian@kcl.ac.uk>
2017-11-08 19:54:09 +0000

    api + css

--- my_app.py
+++ my_app.py
@@ -12,7 +12,7 @@ def code_to_capital(code):
 def what_is_weather(location):
 	endpoint = "http://api.openweathermap.org/data/2.5/weather"
 	payload = {"q": location, "units":"metric", "appid":"3b503bfa7ea64695d425e95faf892625"}
-	response = requests.get(endpoint, params=payload)
+	response = requests.get(endpoint, params = payload)
 	return response.json()["weather"][0]["description"] + '<br/>' + str(response.json()["main"]["temp"]) + '<br/>' + str(response.json()["main"]["temp_min"]) + '<br>' + str(response.json()["main"]["temp_max"])   
 
 def what_is_traffic (location):
@@ -21,7 +21,6 @@ def what_is_traffic (location):
 	response = requests.get(endpoint,params=payload)
 	return response.json() 
 
-	
 @app.route("/")
 def hello():
 	return render_template("main.html")
@@ -30,30 +29,15 @@ def hello():
 def countrydetail(location):
 	capital = code_to_capital(location)
 	weather = what_is_weather(capital)
-<<<<<<< HEAD
 	traffic = what_is_traffic(location)
-	return render_template("countrydetail.html", capital = capital, weather = weather)
-=======
-	news = country_news(location) 
-	return render_template("countrydetail.html", capital = capital, weather = weather, news = news)
->>>>>>> origin/master
+	return render_template("countrydetail.html", capital = capital, weather = weather, traffic = traffic)
 
 @app.route("/weather/<location>")
 def weather(location):
 	capital = code_to_capital(location)
 	return str(what_is_weather(capital))
 
-def country_news(location):
-	endpoint = "https://newsapi.org/v1/sources?apiKey=80fb8990d33c49128aab4e2a2990583b&country=" + location
-	response = requests.get(endpoint)
-	return response.json() 
 
-
-
-#API for exchange rates
-#20580abfe5aa991c3c0c67bf5b6c3f25
-
-#API for news: 80fb8990d33c49128aab4e2a2990583b
 
 app.run(debug=True)
 
