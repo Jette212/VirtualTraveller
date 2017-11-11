1a54369c86dab33f42d432b9fe95168368080ebd
Furnisha <furnisha_lycett@hotmail.co.uk>
2017-11-11 21:18:36 +0000

    removed </br> from weather section

--- my_app.py
+++ my_app.py
@@ -14,7 +14,8 @@ def what_is_weather(location):
 	endpoint = "http://api.openweathermap.org/data/2.5/weather"
 	payload = {"q": location, "units":"metric", "appid":"3b503bfa7ea64695d425e95faf892625"}
 	response = requests.get(endpoint, params=payload)
-	return response.json()["weather"][0]["description"] + '<br/>' + str(response.json()["main"]["temp"]) + '<br/>' + str(response.json()["main"]["temp_min"]) + '<br>' + str(response.json()["main"]["temp_max"])   
+	return response.json()["weather"][0]["description"] + '\n' + str(response.json()["main"]["temp"]) + '\n' + str(response.json()["main"]["temp_min"]) + '\n' + str(response.json()["main"]["temp_max"])   
+	
 
 def what_is_traffic (location):
 	endpoint = "http://dev.virtualearth.net/REST/v1/Traffic/Incidents/mapArea/includeLocationCodes?severity=severity1,severity2,severityn&type=type1,type2,typen&key=BingMapsKey"+ location
@@ -26,6 +27,7 @@ def country_news(location):
 	endpoint = "https://newsapi.org/v1/sources?apiKey=80fb8990d33c49128aab4e2a2990583b&country=" + location
 	response = requests.get(endpoint)
 	return response.json()
+
 
 @app.route("/")
 def hello():
@@ -39,7 +41,7 @@ def countrydetail(location):
 	news = country_news(location) 
 	pretty_news = json2html.convert(json = news)
 	return render_template("countrydetail.html", capital = capital, weather = weather, news = pretty_news, traffic = traffic)
-	
+
 @app.route("/weather/<location>")
 def weather(location):
 	capital = code_to_capital(location)
@@ -53,8 +55,6 @@ def weather(location):
 #20580abfe5aa991c3c0c67bf5b6c3f25
 
 #API for news: 80fb8990d33c49128aab4e2a2990583b
-
-#API for photos:AIzaSyCHUm9wiY-mvw8WkGpXeuKZguy_TSP1TtE
 
 app.run(debug=True)
 
